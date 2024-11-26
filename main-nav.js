d5be150bb8d046c3ed7d973f753238ab4cb75448
Kavin Prasath S <141269905+stark-kavin@users.noreply.github.com>
2024-11-26 06:58:17 +0530

    Add files via upload

--- main-nav.js
+++ main-nav.js
@@ -1,19 +1,28 @@
-var toast = new Notyf();
+selectValueChange(document.querySelector("#select-cart"));
 
-document.querySelector("select").addEventListener("change",function() {
-    var container = this.parentNode;
+function selectValueChange(select) {
+    var container = select.parentNode;
+    var sub_total = document.getElementById("sub_total");
 
-    if(this.value == 1){
+    sub_total.innerHTML = Number(select.value) * 275;
+    if(select.value == 1){
         container.querySelector("button.minus-btn").dataset.delete = true;
     }else{
         container.querySelector("button.minus-btn").dataset.delete = false;
     }
-});
+}
+
+document.querySelector(".cart-page").addEventListener("click",function(e) {
+    if(e.target == document.querySelector(".cart-page")){
+        document.getElementById("close").checked = true
+    }
+})
 
 function changeCartItemQuantity(button,change) {
     var container = button.parentElement;
     var select = container.querySelector("select");
     var max = Number(select.dataset.max);
+    var toast = new Notyf();
 
     if(change<0){
         if(Number(select.value)<=1){
@@ -33,11 +42,7 @@ function changeCartItemQuantity(button,change) {
             select.value = Number(select.value) + 1
         }
     }
-    
-    if(select.value == 1){
-        container.querySelector("button.minus-btn").dataset.delete = true;
-    }else{
-        container.querySelector("button.minus-btn").dataset.delete = false;
-    }
+
+    selectValueChange(select)
 
 }
